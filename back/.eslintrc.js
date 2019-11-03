db3183c1e5182f028e90d440a7a9dc4c9a06739a
Denis  Pereira Alves <denis@MacBook-Pro-de-Denis.local>
2019-11-03 16:46:02 -0300

    Configurando ESLint

--- back/.eslintrc.js
+++ back/.eslintrc.js
@@ -1,15 +1,9 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
   env: {
     es6: true,
     node: true
   },
-  plugins: [
-    '@typescript-eslint'
-  ],
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
     'standard'
   ],
   globals: {
